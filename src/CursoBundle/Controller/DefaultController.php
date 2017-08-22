3d32f945b6cdef30bb9804dcbdc61df94cf585c3
Sandra Monzon <sandra.monzon@tmtfactory.com>
2017-08-22 10:05:37 +0200

    CREATE: Create basic project with bundle CursoBundle. [TEMA 1]

---
+++ src/CursoBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CursoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CursoBundle:Default:index.html.twig');
+    }
+}
