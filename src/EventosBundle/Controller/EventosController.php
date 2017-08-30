39bea2de53c1279196bb4c3f3cf5c791fede2715
Sandra Monzon <sandra.monzon@tmtfactory.com>
2017-08-30 11:02:52 +0200

    FIX: Solved routing config, eventos/1 working [TEMA 2]

--- src/EventosBundle/Controller/EventosController.php
+++ src/EventosBundle/Controller/EventosController.php
@@ -3,17 +3,30 @@
 namespace EventosBundle\Controller; 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller; 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
 class EventosController extends Controller { 
 
- public function showAction($slug)  { 
+  public function showAction($id)
+  {
+    $repository = $this->getDoctrine()->getRepository('EventosBundle:Evento');
+   	$evento = $repository->find($id);
 
-   $recetas = // usa la variable $slug para consultar la base de datos return 
+   /*$recetas = // usa la variable $slug para consultar la base de datos return 
    // COMO¿? En el tema 1 php bin/console doctrine:schema:create dice que no hay metadata to process. Y en la documentacion no mencionais nada de como funciona slug.
 
-   $this->render('EventosBundle:Eventos:show.html.twig', array('eventos' => $eventos, )); 
+   $this->render('EventosBundle:Eventos:show.html.twig', array('eventos' => $eventos, )); */
+  }
 
-  } 
+    /**
+     * @Route("/eventos")
+     */
+    public function indexAction()
+    {
+        return $this->render('EventosBundle:Default:index.html.twig');
+    }
+
 
 } 
